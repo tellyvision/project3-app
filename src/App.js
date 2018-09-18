40013f832e9498687bb90d7550e1ceab2dbb379a
unknown <victorha@gmail.com>
2018-09-18 14:37:33 -0400

    need to add home/search/login components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Wrapper from "./components/Wrapper";
+import Navbar from "./components/Navbar/Navbar";
+import Wrapper from "./components/Wrapper/Wrapper";
 
 import Home from "./pages/Home";
 import Search from "./pages/Search";
 import Login from "./pages/Login";
 
 //import Footer from "./components/Footer";
-import logo from './logo.svg';
+//import logo from './logo.svg';
 //import './App.css';
 
 //class App extends Component {
@@ -23,15 +23,6 @@ const App = () => (
             <Route exact path="/search" component={Search} />
             <Route exact path="/login" component={Login} />
           </Wrapper>
-          {/* <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <h1 className="App-title">Welcome to React</h1>
-            </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </div> */}
           {/* <Footer /> */}
         </div>
       </Router>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom";
 const Navbar = props => (
     
     //todo: make links into <Links>
-    <nav className="navbar navbar-light" style="background-color: #e3f2fd;">
-    <a className="navbar-brand" href="#">Dogs</a>
+    <nav className="navbar navbar-light" containerStyle="background-color: #e3f2fd;">
+    <Link to ="/" className="navbar-brand">Dogs</Link>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
         <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">Home</li>
-            <li className="nav-item">Search</li>
-            <li className="nav-item">Log-in</li>
+            <li className="nav-item active"><Link to ="/">Home</Link></li>
+            <li className="nav-item"><Link to ="/search">Search</Link></li>
+            <li className="nav-item"><Link to ="/login">Log In</Link></li>
         </ul>
     </nav>
 )
