d40e4b81f5f8353afb248ac3bb2b2be718876cfa
thy <doanthy.vo@gmail.com>
2018-09-23 19:07:53 -0400

    simple passport auth with mongo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,36 +1,70 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import axios from 'axios'
 
 class Navbar extends Component {
-    constructor(props) {
-        super(props);
-    }
+	constructor(props) {
+		super(props);
+		this.logout = this.logout.bind(this)	
+	}
 
+	logout(event) {
+		event.preventDefault()
+		console.log('logging out')
+		axios.post('/user/logout').then(response => {
+			console.log(response.data)
+			if (response.status === 200) {
+				this.props.updateUser({
+					loggedIn: false,
+					username: null
+				})
+			}
+		}).catch(error => {
+			console.log('Logout error')
+		})
+	}
     
+	render() {
+		const loggedIn = this.props.loggedIn;
+		console.log('navbar render, props: ')
+		console.log(this.props);
 
-    render() {
-        return(
-        <nav className="navbar navbar-expand-lg navbar-light bg-light" containerStyle="background-color: #e3f2fd">
-            <Link to ="/" className="navbar-brand">Dogs</Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav mr-auto">
-                    <li className={window.location.pathname === "/"
-                ? "nav-item nav-link active"
-                : "nav-item nav-link"}><Link to ="/">Home</Link></li>
-                    <li className={window.location.pathname === "/search"
-                ? "nav-item nav-link active"
-                : "nav-item nav-link"}><Link to ="/search">Search</Link></li>
-                    <li className={window.location.pathname === "/login"
-                ? "nav-item nav-link active"
-                : "nav-item nav-link"}><Link to ="/login">Log In</Link></li>
-                </ul>
-            </div>
-        </nav>
-        )
-    }
+		return(
+		<nav className="navbar navbar-expand-lg navbar-light bg-light" containerStyle="background-color: #e3f2fd">
+				<Link to ="/" className="navbar-brand">Dogs</Link>
+
+				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+				<span className="navbar-toggler-icon"></span>
+				</button>
+
+				<div className="collapse navbar-collapse" id="navbarNav">
+					<ul className="navbar-nav mr-auto">
+						
+						<li className={window.location.pathname === "/"
+						? "nav-item nav-link active"
+						: "nav-item nav-link"}><Link to ="/">Home</Link>
+						</li>
+						
+						<li className={window.location.pathname === "/search"
+						? "nav-item nav-link active"
+						: "nav-item nav-link"}><Link to ="/search">Search</Link>
+						</li>
+						
+						<li className={window.location.pathname === "/login"
+						? "nav-item nav-link active"
+						: "nav-item nav-link"}><Link to ="/login">Log In</Link>
+						</li>
+		
+						<li className={window.location.pathname === "/userRegister"
+						? "nav-item nav-link active"
+						: "nav-item nav-link"}><Link to ="/userRegister">Register</Link>
+						</li>
+		
+					</ul>
+				</div>
+		</nav>
+		)
+	}
     
 }
 
---
+++ src/history/createBrowserHistory.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import React, { Component }  from 'react';
+
+const createHistory = (props) => {
+    return(
+        <div>
+         temp.
+        </div>
+    )
+}
+
+export default createHistory;
