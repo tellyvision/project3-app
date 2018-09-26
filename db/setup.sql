7942d1716ec1223e9a13b500a8b84eaa0492ed7d
Xinyi Zhu <XinyiZhu@Xinyis-MacBook-Pro.local>
2018-09-26 16:25:51 -0400

    added dummy data code in setup.sql

--- db/setup.sql
+++ db/setup.sql
@@ -35,3 +35,12 @@ CREATE TABLE IF NOT EXISTS bookingInfo(
     foreign key fk_lender(lender_id) references userInfo(user_id),
     foreign key fk_borrower(borrower_id) references userInfo(user_id)
 );
+
+-- dummy data
+insert into userInfo (user_name, user_password, user_email, user_addr, user_postal) values ("Xinyi Zhu", "1111", "xinyizhu@gmail.com", "toronto", "m5v3y4"), ("Victor Ha", "2222", "victor@gmail.com", "somewhere", "m3f5o5"), ("Thy Vo", "3333", "thy@gmail.com", "earth", "m2e8t9");
+
+insert into dogInfo(owner_id, dog_name, dog_url, size, breed, activeness, microchip, social_children, social_ppl, social_dog) values (1, "kuro", "xxxx", 3, "retriever", 5, true, 3, 4, 3);
+insert into dogInfo(owner_id, dog_name, dog_url, size, breed, activeness, microchip, social_children, social_ppl, social_dog) values (1, "shiro", "xxxx", 1, "teddy", 4, false, 2, 3, 3);
+insert into dogInfo(owner_id, dog_name, dog_url, size, breed, activeness) values (6, "jelly", "xxxx", 3, "german shepherd", 4);
+
+insert into bookingInfo (booking_dog_id, lender_id, borrower_id, booking_date) values (1, 1, 7, "2018-10-11"), (1, 1, 6, "2018-10-14"), (4, 6, 1, "2018-10-12");
