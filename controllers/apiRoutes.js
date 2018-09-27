8b9d8f2ae8d993ae9a9f6db719f62beb44cbade6
Xinyi Zhu <XinyiZhu@Xinyis-MacBook-Pro.local>
2018-09-27 19:26:33 -0400

    my dog page added

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -11,9 +11,7 @@ module.exports = function(app){
     app.get('/api/search*', function(req, res) {
         console.log(req.query.column);
         console.log(req.query.columnVar);
-        // console.log(req.params);
-        //str = str.replace(/"/g,"")
-        
+
             let SQLquery = "select * from dogInfo where " + req.query.column + " = " + req.query.columnVar;    
 
             connection.query(SQLquery, (err, data) => {
@@ -21,19 +19,12 @@ module.exports = function(app){
                     console.log(err);
                     return false;
                 }
-                // resolve(data);
-                // res.json(data);  
-                
-            
-                res.json(data)
-        
+                res.json(data);
         });
-        // let query = `select * from dogInfo where ${connection.escape(req.query.column)} = ${connection.escape(req.query.columnVal)}`;    
-        
     });
 
-    // get request for get dog info
-    app.get('/api/dog-info', function(req, res) {
+    // get request for get 1 dog info
+    app.get('/api/dog-info/:id', function(req, res) {
         let query = `select * from dogInfo where dog_id = ${req.body}`;
         connection.query(query, (err, data) => {
             if(err) {
@@ -43,6 +34,21 @@ module.exports = function(app){
             return true;
         })
     });
+
+
+    // get request for getting all dogs info owned a specific user
+    app.get('/api/ownerDogSearch', function(req, res) {
+        let query = "select * from dogInfo where owner_id = " + req.query.userID;
+        connection.query(query, (err, data) => {
+            if(err) {
+                console.log(err);
+                return false;
+            }
+            console.log(data);
+            res.json(data);
+        });
+    });
+
 
     // get request for user information, data used to populate form
     app.get('/api/user-profile', function(req, res) {
@@ -108,7 +114,7 @@ module.exports = function(app){
     });
 
     // modifying existing dog profile
-    app.put('/api/dog-profile/:id', function(req, res) {
+    app.put('/dog-profile/:id', function(req, res) {
         var query = `update dogInfo () values () where dog_id = ${req.params.id}`;
         connection.query(query, (err, data) => {
             if(err) {
@@ -120,7 +126,7 @@ module.exports = function(app){
     });
 
     // updating existing/unfulfilled meetup - for borrower
-    app.put('/api/meet-up/:id', function(req, res) {
+    app.put('/meet-up/:id', function(req, res) {
         var query = `update userInfo (lender_id) values () where booking_id = ${req.params.id}`;
         connection.query(query, (err, data) => {
             if(err) {
@@ -136,39 +142,39 @@ module.exports = function(app){
     /////////////////////
 
     // delete a user profile
-    app.delete('/api/user-profile/:id', function(req, res) {
-        var query = `delete from userInfo where booking_id = ${req.params.id}`;
-        connection.query(query, (err, data) => {
-            if(err) {
-                console.log(err);
-                return false;
-            }
-            return true;
-        });
-    });
+    // app.delete('/api/user-profile/:id', function(req, res) {
+    //     var query = `delete from userInfo where booking_id = ${req.params.id}`;
+    //     connection.query(query, (err, data) => {
+    //         if(err) {
+    //             console.log(err);
+    //             return false;
+    //         }
+    //         return true;
+    //     });
+    // });
 
     // delete a dog profile
-    app.delete('/api/dog-profile/:id', function(req, res) {
-        var query = `delete from dogInfo where booking_id = ${req.params.id}`;
-        connection.query(query, (err, data) => {
-            if(err) {
-                console.log(err);
-                return false;
-            }
-            return true;
-        });
-    });
+    // app.delete('/api/dog-profile/:id', function(req, res) {
+    //     var query = `delete from dogInfo where booking_id = ${req.params.id}`;
+    //     connection.query(query, (err, data) => {
+    //         if(err) {
+    //             console.log(err);
+    //             return false;
+    //         }
+    //         return true;
+    //     });
+    // });
 
     // delete a meet-up entry
-    app.delete('/api/meet-up/:id', function(req, res) {
-        var query = `delete from bookingInfo where booking_id = ${req.params.id}`;
-        connection.query(query, (err, data) => {
-            if(err) {
-                console.log(err);
-                return false;
-            }
-            return true;
-        });
-    });
+    // app.delete('/api/meet-up/:id', function(req, res) {
+    //     var query = `delete from bookingInfo where booking_id = ${req.params.id}`;
+    //     connection.query(query, (err, data) => {
+    //         if(err) {
+    //             console.log(err);
+    //             return false;
+    //         }
+    //         return true;
+    //     });
+    // });
 
 }
--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ import ProfilePerson from "./pages/ProfilePerson";
 import DogRegister from "./pages/DogRegister";
 import DogWalkerBook from "./pages/ProfileDogWalker";
 // import DogOwnerBooking from "./pages/ProfileDogOwner";
+import ProfilePersonDogListing from "./pages/ProfilePersonDogListing";
+// import ProfileDogOwner from "./pages/ProfileDogOwner";
 
 const history = createHistory();
 
@@ -37,6 +39,8 @@ class App extends Component {
     this.componentDidMount = this.componentDidMount.bind(this)
     this.updateUser = this.updateUser.bind(this) 
     this.updateListFromSearch = this.updateListFromSearch.bind(this);
+    this.getOwnerDogList = this.getOwnerDogList.bind(this);
+
   }
 
   componentDidMount() {
@@ -66,6 +70,26 @@ class App extends Component {
         })
       }
     })
+  }
+
+  getOwnerDogList() {
+    if(this.state.loggedIn === true && this.state.username !== "") {
+      axios.get('http://localhost:3001/api/ownerDogSearch', {
+            params:{
+                userID: this.props.user_id
+            }
+        })
+        .then((res) => {
+            console.log("res owner's dogs: ")
+            console.log(res);
+            let owner_dog_list = res.data;
+            console.log("new owner's doglist state: ");
+            console.log(this.state.ownerDogList);
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+    }
   }
 
   updateListFromSearch(dataFromSearch) {
@@ -100,7 +124,9 @@ class App extends Component {
               />
               <Route exact path="/profile" component={ProfilePerson} />
               <Route path="/*/register" render={(props) => <DogRegister {...props} user_id = {this.state.user_id} />} />
+              <Route exact path="/your-dog-listing" render={(props) => <ProfilePersonDogListing {...props} user_id = {this.state.user_id} user_dog_list = {this.owner_dog_list}/>} />
               <Route path="/dog-info/*" render={(props) => <DogWalkerBook {...props} picture = "https://ichef.bbci.co.uk/news/660/cpsprodpb/1999/production/_92935560_robot976.jpg" dog_name = "testName" size= "testSize" breed = "testBreed" activeness = "testActiveness" microchip = "1" social_children = "testChildren" social_ppl = "testPeople" social_dog = "testDog" dog_id="testID"/>} />
+              
             </Switch>
           {/* <Footer /> */}  
         </div>
